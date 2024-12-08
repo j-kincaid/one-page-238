cdeed49146da2dfa3becf9e5064cd0fa8d3be8c0
Jessica Kincaid <4j.kincaid@gmail.com>
2024-12-08 16:50:21 -0600

    add separate images of infographic, alternate image of glasses, update css

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-// $(document).ready(function(){
-//     $('#toggleIntro').hover(function(){
-//       $('#intro').slideToggle('slow');
-//     });
-//   });
-//   $(document).ready(function(){
-//     $('#toggleIng').hover(function(){
-//       $('#ing').slideToggle('slow');
-//     });
-//   });
 
-
-//   $(document).ready(function(){
-//     $('#toggleVis').hover(function(){
-//       $('#vis').slideToggle('slow');
-//     });
-//   });
-
-//   $(document).ready(function(){
-//     $('#toggleComp').hover(function(){
-//       $('#comp').slideToggle('slow');
-//     });
-//   });
+// Accordion
 
 $(document).ready(function(){
-    $('#menu ul > li ul')
-      .click(function(event){
-        event.stopPropagation();
-      })
-      .filter(':not(:first)')
-      .hide();
-      
-    $('#menu ul > li').click(function(){
-      var selfClick = $(this).find('ul:first').is(':visible');
-      if(!selfClick) {
-        $(this)
-          .parent()
-          .find('> li ul:visible')
-          .slideToggle(slow);
-      }
-      
+  $('#navigation ul > li ul')
+    .click(function(event){
+      event.stopPropagation();
+    })
+    .filter(':not(:first)')
+    .hide();
+    
+  $('#navigation ul > li').click(function(){
+    var selfClick = $(this).find('ul:first').is(':visible');
+    if(!selfClick) {
       $(this)
-        .find('ul:first')
-        .stop(true, true)
+        .parent()
+        .find('> li ul:visible')
         .slideToggle();
-    });
+    }
+    
+    $(this)
+      .find('ul:first')
+      .stop(true, true)
+      .slideToggle();
   });
-  
+});
